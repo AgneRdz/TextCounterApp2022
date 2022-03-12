76030415103ed4fae35beea8ed0e9da27e1c07be
AgneRdz <agne.radz@gmail.com>
2022-03-13 00:04:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/textcounterapp2022/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.textcounterapp2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.textcounterapp2022.utils.TextUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    EditText txtMain;
+    Spinner spOptionSelection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tvResult = findViewById(R.id.tvResult);
+        this.txtMain = findViewById(R.id.txtMain);
+        this.spOptionSelection = findViewById(R.id.spOptionSelection);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.counting_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spOptionSelection.setAdapter(adapter);
+    }
+
+    public void onBtnCalculateClick(View view) {
+        if (false) {
+            if (this.spOptionSelection.getSelectedItem().toString().equalsIgnoreCase("Words")) {
+                //TODO:
+            } else {
+                int charsCount = TextUtils.getCharsCount(this.txtMain.getText().toString());
+                String charsCountFormatted = String.valueOf(charsCount);
+                this.tvResult.setText(charsCountFormatted);
+            }
+        } else {
+            Toast.makeText(this, "Text is empty", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/textcounterapp2022/utils/TextUtils.java
@@ -0,0 +1,7 @@
+package com.example.textcounterapp2022.utils;
+
+public class TextUtils {
+    public static int getCharsCount(String input){
+        return input.length();
+    }
+}
