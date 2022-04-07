62165fd96eba2d8a1eef15c8688acdf42cd54dd5
AgneRdz <agne.radz@gmail.com>
2022-04-07 22:11:58 +0300

    Updated text counting function

--- app/src/main/java/com/example/textcounterapp2022/MainActivity.java
+++ app/src/main/java/com/example/textcounterapp2022/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tvResult;
     EditText txtMain;
     Spinner spOptionSelection;
+    String wordsSelected;
+    String emptyTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         this.tvResult = findViewById(R.id.tvResult);
         this.txtMain = findViewById(R.id.txtMain);
         this.spOptionSelection = findViewById(R.id.spOptionSelection);
+        this.wordsSelected = getResources().getString(R.string.words_selected);
+        this.emptyTextMessage = getResources().getString(R.string.empty_text_message);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.counting_options, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -33,16 +37,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnCalculateClick(View view) {
-        if (false) {
-            if (this.spOptionSelection.getSelectedItem().toString().equalsIgnoreCase("Words")) {
-                //TODO:
-            } else {
+        if(txtMain.getText().toString().isEmpty() ){
+            Toast.makeText(this, emptyTextMessage, Toast.LENGTH_LONG).show();
+        } else{
+            if(this.spOptionSelection.getSelectedItem().toString().equalsIgnoreCase(wordsSelected)){
+                int wordsCount = TextUtils.getWordsCount(this.txtMain.getText().toString());
+                String wordsCountFormatted = String.valueOf(wordsCount);
+                this.tvResult.setText(wordsCountFormatted);
+            } else{
                 int charsCount = TextUtils.getCharsCount(this.txtMain.getText().toString());
                 String charsCountFormatted = String.valueOf(charsCount);
                 this.tvResult.setText(charsCountFormatted);
             }
-        } else {
-            Toast.makeText(this, "Text is empty", Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/example/textcounterapp2022/utils/TextUtils.java
+++ app/src/main/java/com/example/textcounterapp2022/utils/TextUtils.java
@@ -3,5 +3,11 @@ package com.example.textcounterapp2022.utils;
 public class TextUtils {
     public static int getCharsCount(String input){
         return input.length();
+
     }
+    public static int getWordsCount(String input){
+        String words = input.trim();
+        return words.split("\\s+").length;
+    }
+
 }
